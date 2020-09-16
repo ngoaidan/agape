03ce2bd936224ea34ca33f915a7bb1a8bfa70094
ngoaidan <cau2binhdinh@gmail.com>
2020-09-16 18:01:10 +0700

    create orders table

---
+++ database/migrations/2020_09_16_102447_create_orders_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('customer_id');
+            $table->smallInteger('status');
+            $table->unsignedInteger('product_id');
+            $table->timestamps();
+
+            $table->foreign('customer_id')->references('id')->on('customers');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('orders', function (Blueprint $table) {
+            $table->dropForeign('orders_customer_id_foreign');
+        });
+        Schema::dropIfExists('orders');
+    }
+}
