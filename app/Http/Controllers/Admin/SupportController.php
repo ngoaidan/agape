765156730ee072268bdc146e8d4a70b49335a47d
ngoaidan <cau2binhdinh@gmail.com>
2020-10-20 17:13:34 +0700

    support addmin

---
+++ app/Http/Controllers/Admin/SupportController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class SupportController extends AdminController
+{
+    //
+}
