804503c9613518fc82150bd1b9331c7d15799aa0
Giang <jun154607@gmail.com>
2021-01-12 17:57:17 +0700

    ##

--- resources/views/vendor/voyager/products/edit-add.blade.php
+++ resources/views/vendor/voyager/products/edit-add.blade.php
@@ -333,4 +333,14 @@
     </script>
     <script src="https://cdn.tiny.cloud/1/qpdlbwg6bqiob1m9hf8vffx0daac29tlyvxz59i58c201g3j/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
     <script>tinymce.init({selector:'textarea'});</script>
+    <script>
+        tinymce.init({
+            selector: 'textarea',
+            plugins: 'a11ychecker advcode casechange formatpainter linkchecker autolink lists checklist media mediaembed pageembed permanentpen powerpaste table advtable tinycomments tinymcespellchecker',
+            toolbar: 'a11ycheck addcomment showcomments casechange checklist code formatpainter pageembed permanentpen table',
+            toolbar_mode: 'floating',
+            tinycomments_mode: 'embedded',
+            tinycomments_author: 'Author name',
+        });
+    </script>
 @stop
